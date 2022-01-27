d6fd79c3a49cd3694792eadae7798aeb824b106b
Artem L <artuon@gmail.com>
2022-01-27 20:29:47 +0200

    Move out fetch to a module

---
+++ src/fetch.js
@@ -0,0 +1,41 @@
+import {Queue} from "./classes/lists.js";
+import {getJsonPlaceholder} from "./boundaries.js";
+import {ATTEMPTS, PORTION_LENGTH, QUEUE_LENGTH} from "./constants.js";
+
+export const initQueue = () => {
+  for (let i = 1; i <= QUEUE_LENGTH; i++) {
+    Queue.addEntity(i)
+  }
+}
+const getPortion = () => {
+  return Queue.list.filter(
+    (e) => (!e.result || e.attempts >= ATTEMPTS)).slice(0, PORTION_LENGTH)
+}
+export const fetchData = async () => {
+  console.log('--- Starting fetch ---')
+  console.log('Queue before:', Queue.list)
+  let portion
+  portion = getPortion()
+
+  while (portion.length > 0) {
+
+    const portionPromises = portion.map(async (e) => {
+      return getJsonPlaceholder(e.id)
+    })
+
+    const response = await Promise.all(portionPromises)
+
+    response.forEach(
+      (e) => {
+        const ind = Queue.findEntityIndex(e.id)
+        if (ind !== -1) {
+          Queue.list[ind].attempt()
+          Queue.list[ind].fill(e.body)
+        }
+      }
+    )
+
+    portion = getPortion()
+  }
+  console.log('Queue after:', Queue.list)
+}
--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
-import {Queue} from "./classes/lists.js";
-import {ATTEMPTS, PORTION_LENGTH, QUEUE_LENGTH} from "./constants.js";
-import {getJsonPlaceholder} from "./boundaries.js";
+import {fetchData, initQueue} from "./fetch.js";
 
-for (let i = 1; i <= QUEUE_LENGTH; i++) {
-  Queue.addEntity(i)
-}
 
-const getPortion = () => {
-  return Queue.list.filter(
-    (e) => (!e.result || e.attempts >= ATTEMPTS)).slice(0, PORTION_LENGTH)
-}
-
-const fetchData = async () => {
-  console.log('--- Starting fetch ---')
-  console.log('Queue before:', Queue.list)
-  let portion
-  portion = getPortion()
-
-  while (portion.length > 0) {
-
-    const portionPromises = portion.map(async (e) => {
-      return getJsonPlaceholder(e.id)
-    })
-
-    const response = await Promise.all(portionPromises)
-
-    response.forEach(
-      (e) => {
-        const ind = Queue.findEntityIndex(e.id)
-        if (ind !== -1) {
-          Queue.list[ind].attempt()
-          Queue.list[ind].fill(e.body)
-        }
-      }
-    )
-
-    portion = getPortion()
-  }
-  console.log('Queue after:', Queue.list)
-}
-
-setInterval(fetchData, 60 * 60 * 1000);
+initQueue()
+fetchData()
+//setInterval(fetchData, 60 * 60 * 1000);
