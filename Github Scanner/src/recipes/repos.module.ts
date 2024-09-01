ef72ee7cab44e3af0a23126e86309f0d7c1efc6c
Artu <artuon@gmail.com>
2024-09-01 16:11:24 +0200

    Remove unfinished

--- Github Scanner/src/recipes/repos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DateScalar } from '../common/scalars/date.scalar';
-import { ReposResolver } from 'src/recipes/repos.resolver';
-import { ReposService } from 'src/recipes/repos.service';
-
-@Module({
-  providers: [ReposResolver, ReposService, DateScalar],
-})
-export class ReposModule {}
